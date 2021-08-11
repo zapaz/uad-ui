9a0615990677a88c70a5af3ba8efde9e0adb1828
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-08-11 14:03:42 -0400

    fix(build): press key to continue prompt breaking this script

--- hooks/build.sh
+++ hooks/build.sh
@@ -17,23 +17,25 @@ else
   echo "Please add a .env inside the contracts folder."
   exit 1
 fi
-rm -f ./frontend/src/uad-contracts-deployment.json
+
+DEPLOYMENT_ARTIFACT=uad-contracts-deployment.json
+
+rm -f ./frontend/src/$DEPLOYMENT_ARTIFACT
 mkdir -p  ./frontend/src
 yarn stop # kill blockchain
 cd ./contracts || echo "ERROR: ./contracts/ doesn't exist?"
 yarn
-export TS_NODE_TRANSPILE_ONLY=1 && yarn hardhat compile
-yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/"$ALCHEMY_API_KEY" --fork-block-number 12150000 --show-accounts --export-all tmp-uad-contracts-deployment.json >../local.node.log 2>&1 &
-echo "Pausing until uad-contracts-deployment.json exists."
+export TS_NODE_TRANSPILE_ONLY=1 && yarn hardhat compile &>../hardhat-compile.log 2>&1 &
+yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/"$ALCHEMY_API_KEY" --fork-block-number 12150000 --show-accounts --export-all tmp-$DEPLOYMENT_ARTIFACT >../local.node.log 2>&1 &
+echo "Pausing until $DEPLOYMENT_ARTIFACT exists."
 while :; do
   spinnerSTART
-  [[ -f "tmp-uad-contracts-deployment.json" ]] && break
+  [[ -f "tmp-$DEPLOYMENT_ARTIFACT" ]] && break
   sleep .06
 done
 spinnerSTOP
-node ../hooks/process-deployment.js ./tmp-uad-contracts-deployment.json ../frontend/src/uad-contracts-deployment.json
-rm -f ./tmp-uad-contracts-deployment.json
+node ../hooks/process-deployment.js ./tmp-$DEPLOYMENT_ARTIFACT ../frontend/src/$DEPLOYMENT_ARTIFACT
+rm -f ./tmp-$DEPLOYMENT_ARTIFACT
 cd ..
-mkdir -p  ./frontend/src/types && cp -r ./contracts/artifacts/types/* ./frontend/src/types # copy artifacts to be accessible by frontend
-
+mkdir -p
 exit 0
